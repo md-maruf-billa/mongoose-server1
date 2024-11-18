35511e06627e29a0c01939a9f53ada2760ce4b16
md-maruf-billa <157400283+md-maruf-billa@users.noreply.github.com>
2024-11-19 00:31:29 +0600

    added eslint and prettier

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,11 +1,10 @@
-import dotevn from "dotenv";
+import dotevn from 'dotenv';
 import path from 'path';
 
 // configure dot env
-dotevn.config({ path: path.join(process.cwd() + ".env") });
+dotevn.config({ path: path.join(process.cwd() + '.env') });
 
 const config = {
-    port: process.env.PORT,
-
-}
+  port: process.env.PORT,
+};
 export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,10 @@
 // import app
 import app from './app';
-import config from './app/config/index'
+import config from './app/config/index';
 
 // define port
 const port = 5000;
 
-
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
-
+  console.log(`Example app listening on port ${port}`);
+});
