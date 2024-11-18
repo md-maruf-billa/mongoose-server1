255130c5d8f27c297942f26040d8b6748036a4ec
md-maruf-billa <157400283+md-maruf-billa@users.noreply.github.com>
2024-11-18 22:43:54 +0600

    added server setup file

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+// import app
+import app from './app';
+import config from './app/config/index'
+
+// define port
+const port = 5000;
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+
